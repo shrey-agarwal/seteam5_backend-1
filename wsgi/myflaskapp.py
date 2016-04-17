8e059d802a775a97f62c8216c37350a69f167ba0
shrey-agarwal <shrey.agarwal2@gmail.com>
2016-04-17 17:31:37 +0530

    Added guardian feature

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 
+import cloud_messaging
 import database_driver
+import util
 
 app = Flask(__name__)
 
@@ -54,6 +56,76 @@ def admin_student_status_update():
     database_driver.update_student_info(update)
     return '', 200
 
+
+@app.route('/route_manage.html')
+def manage_route_html():
+    unassigned_students = database_driver.get_unassigned_students()
+    routes = {'A': [],
+              'B': [],
+              'C': [],
+              }
+    for student in database_driver.get_assigned_students():
+        routes[student['route']].append(student)
+    return render_template('route_manage.html',
+                           unassigned_students=unassigned_students,
+                           routes=routes)
+
+
+@app.route('/register_token/', methods=['POST'])
+def register_token():
+    phonenumber = request.form.get('phonenumber')
+    token = request.form.get('token')
+    database_driver.register_token(phonenumber, token)
+    return 'registered successfully', 200
+
+
+@app.route('/send_message/', methods=['POST'])
+def send_message():
+    from_ = request.form.get('phonenumber')
+    message = request.form.get('message')
+    cloud_messaging.send_message(from_, '', message)
+    return jsonify(request.form)
+
+
+@app.route('/google95f9878b0eb7c516.html')
+def verify():
+    return render_template('google94f9878b0eb7c516.html')
+
+
+@app.route('/update_user_location/', methods=['POST'])
+def update_user_location():
+    if 'phonenumber' not in request.form:
+        return 'Missing phonenumber', 400
+    info = database_driver.get_userinfo(request.form['phonenumber'])
+    if not info:
+        return 'user does not exist. register first', 409
+    if 'location' not in request.form:
+        return 'location does not exist', 409
+    phonenumber = request.form.get('phonenumber')
+    location = request.form.get('location')
+    if database_driver.update_user_location(phonenumber, location):
+        return 'updated successfully', 200
+
+
+@app.route('/emergency/', methods=['POST'])
+def send_emergency_update():
+    for guardian in util.get_guardian_phonenumbers():
+        cloud_messaging.send_sms(guardian,
+                                 request.form.get('message'))
+
+    return 'sent', 200
+
+
+@app.route('/add_student_route/', methods=['POST'])
+def add_student_route():
+    phonenumber = request.form.get('phonenumber')
+    route = request.form.get('route')
+    updated_info = {'phonenumber': phonenumber,
+                    'route': route,
+                    }
+    database_driver.update_student_route(updated_info)
+    return ''
+
 @app.route('/guardianLogin', methods=['POST'])
 def add_guardinfo():
     if 'phonenumber' not in request.form:
@@ -75,4 +147,4 @@ def add_guardinfo():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
