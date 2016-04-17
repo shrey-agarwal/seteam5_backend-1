b9c59dcadcc772852ce28f6626d1f312b4097ad1
shrey-agarwal <shrey.agarwal2@gmail.com>
2016-04-17 17:27:40 +0530

    Added guardian feature

--- wsgi/database_driver.py
+++ wsgi/database_driver.py
@@ -28,11 +28,13 @@ def get_userinfo(phonenumber):
         info.pop('_id', None)
     return info
 
-def add_route(phonenumber,route):
-	db = client.backend
-	info={}
-	info[phonenumber] = route
-	return db.routeinfo.insert_one(info)
+
+def add_route(phonenumber, route):
+    db = client.backend
+    info = {}
+    info[phonenumber] = route
+    return db.routeinfo.insert_one(info)
+
 
 def get_students_info(query):
     db = client.backend
@@ -45,6 +47,51 @@ def update_student_info(updated_info):
     db.userinfo.update({'phonenumber': updated_info['phonenumber']},
                        {'$set': {'status': updated_info['status']}},
                        True)
+
+
+def update_student_route(updated_info):
+    db = client.backend
+    db.userinfo.update({'phonenumber': updated_info['phonenumber']},
+                       {'$set': {'route': updated_info['route']}},
+                       True)
+
+
+def get_unassigned_students():
+    db = client.backend
+    rows = db.userinfo.find({"route": {"$exists": False}})
+    return rows
+
+def get_assigned_students():
+    db = client.backend
+    rows = db.userinfo.find({"route": {"$exists": True}})
+    return rows
+
+
+def register_token(phonenumber, token):
+    db = client.backend
+    data = {'phonenumber': phonenumber,
+            'token': token,
+            }
+    db.gcmtokens.update({'phonenumber': phonenumber},
+                        data,
+                        upsert=True)
+
+
+def get_gcm_token(phonenumber):
+    db = client.backend
+    if phonenumber:
+        return db.gcmtokens.find_one({'phonenumber': phonenumber})['token']
+    else:
+        return [i['token'] for i in db.gcmtokens.find()]
+
+
+def update_user_location(phonenumber, location):
+    db = client.backend
+    db.userinfo.update({'phonenumber': phonenumber},
+                       {'$set': {'location': location}},
+                       True)
+    return True
+
 
 def add_guardian(guardinfo):
 	db = client.backend
